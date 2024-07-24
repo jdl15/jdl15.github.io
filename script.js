e3f1f8104a520ad4845d77c2fda036aa80cbc227
danling416 <danling416@gmail.com>
2024-07-23 23:30:11 -0400

    web

---
+++ script.js
@@ -0,0 +1,95 @@
+document.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+});
+
+function handlePress(event) {
+    this.classList.add('pressed');
+}
+
+function handleRelease(event) {
+    this.classList.remove('pressed');
+}
+
+function handleCancel(event) {
+    this.classList.remove('pressed');
+}
+
+var buttons = document.querySelectorAll('.projectItem');
+buttons.forEach(function (button) {
+    button.addEventListener('mousedown', handlePress);
+    button.addEventListener('mouseup', handleRelease);
+    button.addEventListener('mouseleave', handleCancel);
+    button.addEventListener('touchstart', handlePress);
+    button.addEventListener('touchend', handleRelease);
+    button.addEventListener('touchcancel', handleCancel);
+});
+
+function toggleClass(selector, className) {
+    var elements = document.querySelectorAll(selector);
+    elements.forEach(function (element) {
+        element.classList.toggle(className);
+    });
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) == 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var html = document.querySelector('html');
+    var themeState = getCookie("themeState") || "Light";
+
+    function changeTheme(theme) {
+        html.dataset.theme = theme;
+        setCookie("themeState", theme, 365);
+        themeState = theme;
+    }
+    var Checkbox = document.getElementById('myonoffswitch')
+    Checkbox.addEventListener('change', function () {
+        if (themeState == "Dark") {
+            changeTheme("Light");
+        } else if (themeState == "Light") {
+            changeTheme("Dark");
+        } else {
+            changeTheme("Dark");
+        }
+    });
+    if (themeState == "Dark") {
+        Checkbox.checked = false;
+    }
+    changeTheme(themeState);
+});
+
+
+
+
+// var pageLoading = document.querySelector("#zyyo-loading");
+// window.addEventListener('load', function() {
+//     setTimeout(function () {
+//         pageLoading.style.opacity = '0';
+//     }, 100);
+// });
+
